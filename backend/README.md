457575284487db4062f1acfaa32ba0938cb2f842
Daniil Jasper <jasper@deeplay.io>
2025-08-10 22:31:38 +0600

    init project

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+# NextJS-Nest.JS-middlewar
---
+++ backend/next.middleware.ts
@@ -0,0 +1,21 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+import next from 'next';
+
+@Injectable()
+export class NextMiddleware implements NestMiddleware {
+  private nextApp = next({ dev: process.env.NODE_ENV !== 'production', dir: require('path').resolve(__dirname, '../../frontend') });
+  private handle = this.nextApp.getRequestHandler();
+  private prepared = false;
+
+  async use(req: Request, res: Response, nextFn: () => void) {
+    if (!this.prepared) {
+      await this.nextApp.prepare();
+      this.prepared = true;
+    }
+    if (req.url && req.url.startsWith('/api')) {
+      return nextFn();
+    }
+    this.handle(req, res);
+  }
+}
