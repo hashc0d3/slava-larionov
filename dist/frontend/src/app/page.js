1632ad01d8e7ae2fc03580bdfa8c0b9c95d631c3
Daniil Jasper <jasper@deeplay.io>
2025-08-10 22:58:58 +0600

    init: project structure

---
+++ dist/frontend/src/app/page.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Home;
+function Home() {
+    return (<div>
+      Hello World!
+    </div>);
+}
+//# sourceMappingURL=page.js.map
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,19 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+    const [greeting, setGreeting] = useState('');
+
+    useEffect(() => {
+        fetch('/api/greeting')
+            .then(res => res.json())
+            .then(data => setGreeting(data.message));
+    }, []);
+
+    return (
+        <div>
+            {greeting ? greeting : 'Загрузка...'}
+        </div>
+    );
 }
