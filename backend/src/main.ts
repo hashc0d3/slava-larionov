015c7f5ae2fc9b00e7affacb67c055ce9bffeb18
Daniil Jasper <jasper@deeplay.io>
2025-08-20 22:49:26 +0600

    fix: правки

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8081;
   await app.listen(port);
   console.log(`🚀 Server is running on http://localhost:${port}`);
 }
